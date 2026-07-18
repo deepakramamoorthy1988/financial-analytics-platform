601ed860485d8af842ac866fadff22e0e12f7569
Deepak Ramamoorthy <deepakramamoorthy1988@gmail.com>
2026-07-19 00:49:47 +0530

    Day 2 - Added Azure Storage Account using Terraform

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -2,3 +2,20 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+resource "azurerm_storage_account" "storage" {
+
+  name                = var.storage_account_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  account_tier             = var.account_tier
+  account_replication_type = var.replication_type
+
+  is_hns_enabled = true
+
+  tags = {
+    Environment = "Development"
+    Project     = "Financial Analytics"
+  }
+
+}
--- infrastructure/terraform/terraform.tfvars
+++ infrastructure/terraform/terraform.tfvars
@@ -1,3 +1,9 @@
 resource_group_name = "financial-analytics-rg-dev"
 
 location = "Central India"
+
+storage_account_name = "finanalyticsstorage001"
+
+account_tier = "Standard"
+
+replication_type = "LRS"
